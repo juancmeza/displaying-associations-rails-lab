48a0b9836b92ccba660d8660ffbe4f0f9d57d821
juan_carlos_meza <juancmezat@gmail.com>
2020-12-21 01:16:40 -0800

    Something is off

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,2 +1,12 @@
 class Artist < ActiveRecord::Base
+    has_many :songs
+
+    # def all_songs
+    #     Song.all
+    # end
+
+    def song_count
+        #Song.find_by(artist_id: self.id).count
+        self.songs.count
+    end
 end
--- db/migrate/20160108015311_create_songs.rb
+++ db/migrate/20160108015311_create_songs.rb
@@ -2,6 +2,7 @@ class CreateSongs < ActiveRecord::Migration
   def change
     create_table :songs do |t|
       t.string :title
+      belongs_to :artist, null: false, foreign_key: true
 
       t.timestamps null: false
     end
